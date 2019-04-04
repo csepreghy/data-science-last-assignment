361ac427c690f58b4aa9a4ebe94abb8ec11d8132
Andrew Chepreghy <acsepreghy@ganintegrity.com>
2019-04-04 16:50:22 +0200

    created the multivariate linear regression function

--- __init__.py
+++ __init__.py
@@ -1,7 +1,27 @@
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight')
 
 from multivarlinreg import multivarlinreg
+from plotify import Plotify
+
+# Creating instances of imported classes
+plotify = Plotify()
+scaler = StandardScaler()
 
 redwine_training = np.loadtxt('redwine_training.txt')
 
-print('redwine_training', redwine_training)
+
+X_train = redwine_training[:, :-1]
+y_train = redwine_training[:, -1]
+
+X_train_std = scaler.fit_transform(X_train)
+
+# multivarlinreg(X_train_std, y_train)
+weights, cost = multivarlinreg(X_train_std, y_train)
+
+plt.plot(cost)
+plt.show()
